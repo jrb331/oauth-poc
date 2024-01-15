cf92e7f2ff5e18c95733b3113233785ea953b072
jrb331 <jonathanrussellboyd@gmail.com>
2024-01-14 20:40:12 -0500

    add authorizer endpoint

---
+++ api/handlers/authorize.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+exports.authorize = async (event) => {
+  try {
+    response = await axios.post('<placeholder>/oauth2/default/v1/token', 'grant_type=client_credentials&scope=testScope', {
+      headers: {
+          'accept': 'application/json',
+          'authorization': 'Basic <placeholder>',
+          'cache-control': 'no-cache',
+          'content-type': 'application/x-www-form-urlencoded'
+      },
+    });
+
+    let v = ''
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: "Successfully called authorizer",
+            input: response.data,
+          },
+          null,
+          2
+        ),
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+        statusCode: 403,
+        body: JSON.stringify(
+            {
+                message: "Access Denied",
+                error: error.message,
+            },
+            null,
+            2
+        ),
+    };
+  }
+};
